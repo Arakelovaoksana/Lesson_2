bdf3ac21157c5ef6920f738e0a9f7e6436025d37
Arakelova <kuzya_1972_72@mail.ru>
2022-09-21 19:57:59 +0300

    Решили задачу 23

---
+++ dz_23/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+     
+     Console.Clear();
+      Console.Write("Введите кол-во чисел для вывода:");
+            int N = int.Parse(Console.ReadLine()!);
+ 
+            Console.WriteLine("Число" + "\t" + "Куб");
+            for(int i = 1; i <= N; i++)
+                Console.WriteLine(i + "\t" + i*i*i);
+ 
+            Console.ReadLine();
+  
+        
+    
